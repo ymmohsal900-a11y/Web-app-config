cb6028a553624500d1fa7c340ed6f6603c53ba78
ymmohsal900-a11y <ymmohsal900@gmail.com>
2026-04-20 20:01:26 +0530

    Update MSAL1.py

--- MSAL.py
+++ MSAL.py
@@ -1,1 +1,8 @@
 import msal
+def _build_msal_app(cache=None, authority=None):
+    return msal.ConfidentialClientApplication(
+        current_app.config["CLIENT_ID"],
+        authority=authority or current_app.config["AUTHORITY"],
+        client_credential=current_app.config["CLIENT_SECRET"],
+        token_cache=cache,
+    )

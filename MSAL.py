9deb25e77a509ba7b5d3ee0fc42958dd300b8ba3
ymmohsal900-a11y <ymmohsal900@gmail.com>
2026-04-20 20:02:33 +0530

    Update MSALcenter.py

--- MSAL.py
+++ MSAL.py
@@ -1,7 +1,21 @@
-    @app.route("/login")
-def login():
-    session["flow"] = _build_msal_app().initiate_auth_code_flow(
-        scopes=current_app.config["SCOPE"],
-        redirect_uri=url_for("authorized", _external=True)
-    )
-    return redirect(session["flow"]["auth_uri"])
+@app.route(current_app.config["REDIRECT_PATH"])
+def authorized():
+    try:
+        cache = msal.SerializableTokenCache()
+        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
+            session.get("flow", {}),
+            request.args
+        )
+        
+        if "error" in result:
+            current_app.logger.error("Invalid login attempt")
+            return "Login failed"
+
+        session["user"] = result.get("id_token_claims")
+        current_app.logger.info("Login successful")
+
+        return redirect(url_for("index"))
+
+    except Exception as e:
+        current_app.logger.error("Login exception: " + str(e))
+        return "Error occurred"

b9b8d02ff621cfe0588ba36c53f3154c2e87e6ad
ymmohsal900-a11y <ymmohsal900@gmail.com>
2026-04-20 20:02:56 +0530

    Update MSAL_logout.py

--- MSAL.py
+++ MSAL.py
@@ -1,21 +1,7 @@
-@app.route(current_app.config["REDIRECT_PATH"])
-def authorized():
-    try:
-        cache = msal.SerializableTokenCache()
-        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
-            session.get("flow", {}),
-            request.args
-        )
-        
-        if "error" in result:
-            current_app.logger.error("Invalid login attempt")
-            return "Login failed"
-
-        session["user"] = result.get("id_token_claims")
-        current_app.logger.info("Login successful")
-
-        return redirect(url_for("index"))
-
-    except Exception as e:
-        current_app.logger.error("Login exception: " + str(e))
-        return "Error occurred"
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        current_app.config["AUTHORITY"] + "/oauth2/v2.0/logout" +
+        "?post_logout_redirect_uri=" + url_for("index", _external=True)
+    )

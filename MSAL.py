651b8bd6c46c88074fd8bb5274f597912bca5f0b
ymmohsal900-a11y <ymmohsal900@gmail.com>
2026-04-20 20:01:53 +0530

    Update MSAL_login.py

--- MSAL.py
+++ MSAL.py
@@ -1,8 +1,7 @@
-import msal
-def _build_msal_app(cache=None, authority=None):
-    return msal.ConfidentialClientApplication(
-        current_app.config["CLIENT_ID"],
-        authority=authority or current_app.config["AUTHORITY"],
-        client_credential=current_app.config["CLIENT_SECRET"],
-        token_cache=cache,
+    @app.route("/login")
+def login():
+    session["flow"] = _build_msal_app().initiate_auth_code_flow(
+        scopes=current_app.config["SCOPE"],
+        redirect_uri=url_for("authorized", _external=True)
     )
+    return redirect(session["flow"]["auth_uri"])

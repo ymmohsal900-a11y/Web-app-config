51721a9fcbe071b03a08bd86d81fdd449428a3f1
ymmohsal900-a11y <ymmohsal900@gmail.com>
2026-04-20 20:18:07 +0530

    __init__.py

--- MSAL.py
+++
@@ -1,6 +0,0 @@
-CLIENT_ID = os.environ.get("CLIENT_ID")
-CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-
-AUTHORITY = "https://login.microsoftonline.com/common"
-REDIRECT_PATH = "/getAToken"
-SCOPE = ["User.Read"]

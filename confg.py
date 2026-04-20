597bb2b08f9486fcc9a466d17fe4a841eddb8162
ymmohsal900-a11y <ymmohsal900@gmail.com>
2026-04-20 16:59:48 +0530

    Update confg.py

--- confg.py
+++ confg.py
@@ -1,13 +1,2 @@
-import os  
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER', 'images')
-
-    SQL_SERVER = os.environ.get('SQL_SERVER')
-    SQL_DATABASE = os.environ.get('SQL_DATABASE')
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
+SQL_PASSWORD = 'CMS4dmin'
+BLOB_STORAGE_KEY = 'your-storage-key'
